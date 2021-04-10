2f095edd456cb1340a57bd411dddc32e541eb6b2
p0r9 <palathip.opor@gmail.com>
2021-04-10 19:10:21 +0700

    progrram

---
+++ game.py
@@ -0,0 +1,27 @@
+from tkinter import* 
+
+def Gamefuntion(event):
+    CorrectNumber = 132
+    UserGuess = -1
+    while UserGuess != CorrectNumber :
+        UserGuess = int(TextboxGuess.get())
+        if UserGuess > 1000 or UserGuess < 0 :
+            LabelText.configure (text='input nubmer 0-1000 !!!')
+        elif UserGuess > CorrectNumber :
+            LabelText.configure (text='Too Large xD')
+        elif UserGuess < CorrectNumber :
+            LabelText.configure (text='Too Small xD')
+        elif UserGuess == CorrectNumber :
+            LabelText.configure ('Correct!! :D')
+
+MainWindow = Tk()
+LabelGuess = Label(MainWindow,text='ใส่ตัวเลข 1-1000',font=('arial,18'))
+LabelGuess.grid(row=0,column=0)
+TextboxGuess = Entry(MainWindow,width=('16'))
+TextboxGuess.grid(row=1,column=0)
+Enterbutton = Button(MainWindow,text='Enter',font=('arial,30'),width=('12'))
+Enterbutton.grid(row=2,column=0)
+Enterbutton.bind('<Button-1>',Gamefuntion)
+LabelText = Label(MainWindow,text='text',font=('arial,18'))
+LabelText.grid(row=3,column=0)
+MainWindow.mainloop()
--- testgithub.py
+++ testgithub.py
@@ -1,4 +1,7 @@
 p = 1+2+3+4+56+6
 if  p>2:
     print (2)
+    print (3)
+elif p>10 :
+    print (1)
 print ('win')
